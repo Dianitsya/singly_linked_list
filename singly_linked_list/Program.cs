8b916be8e10f762a98edfd91353b39f410242aa9
Dianitsya <dianitasyanandamasta@gmail.com>
2022-11-10 19:00:23 +0700

    add class program

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -121,7 +121,82 @@ namespace singly_linked_list
     {
         static void Main(string[] args)
         {
-           
+            List obj = new List();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Menambah data kedalam list");
+                    Console.WriteLine("2. Menghapus data dari dalam list");
+                    Console.WriteLine("3. Melihat semua data didalam list");
+                    Console.WriteLine("4. Mencari sebuah data didalam list");
+                    Console.WriteLine("5. Exit");
+                    Console.Write("\nMasukkan Pilihan Anda (1-5): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNote();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\nList Kosong");
+                                    break;
+                                }
+                                Console.Write("\nMasukkan nomor mahasiswa yang akan dihapus: ");
+                                int nim = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.delNote(nim) == false)
+                                    Console.WriteLine("\nData tidak ditemukan.");
+                                else
+                                    Console.WriteLine("Data dengan nomor mahasiswa" + nim + " dihapus");
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.treverse();
+                            }
+                            break;
+                        case '4':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList Kosong !");
+                                    break;
+                                }
+                                Node previous, current;
+                                previous = current = null;
+                                Console.Write("\nMasukkan nomor mahasiswa yang akan dicari: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref previous, ref current) == false)
+                                    Console.WriteLine("\nData tidak ditemukan.");
+                                else
+                                {
+                                    Console.WriteLine("\nData Ketemu");
+                                    Console.WriteLine("\nNomor Mahasiswa: " + current.noMhs);
+                                    Console.WriteLine("\nNama: " + current.nama);
+                                }
+                            }
+                            break;
+                        case '5':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("\nPilihan tidak valid");
+                                break;
+                            }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\nCheck nilai yang anda masukkan.");
+                }
+
             }
         }
     }
