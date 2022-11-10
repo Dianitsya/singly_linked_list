2f1a5ebf1a95c1c9c95e042cfc089e687a70798a
Dianitsya <dianitasyanandamasta@gmail.com>
2022-11-10 17:17:32 +0700

    add public List di dalam class List

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -15,6 +15,13 @@ namespace singly_linked_list
     class List
     {
         Node START;
+        public List()
+        {
+            START = null;
+        }
+        //Method untuk menambahkan sebuah node ke dalam list
+
+       
     }
     class Program
     {
